f1c29e6137865cbaf568fa47ebaeae3797a21058
ParthChhetri <117709808+ParthChhetri@users.noreply.github.com>
2023-04-24 11:59:01 +0530

    Add files via upload
    Bike Price prediction uses an algorithm to analyze a product or service based on its characteristics, demand, and current market trends. Then the software sets a price at a level it predicts will both attract customers and maximize sales.

---
+++ myapp.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
+model=pickle.load(open('model.pkl','rb'))
+
+f1=Flask(__name__)
+
+
+@f1.route("/")
+def home():
+    return render_template("index.html")
+
+
+@f1.route("/getpredict",methods=['GET','POST'])
+def predict():
+    features=[float(x) for x in request.form.values()]
+    final_features= [np.array(features)]
+    print(final_features)
+    prediction=model.predict(final_features)
+    output=prediction[0]
+    if output == 0:
+      return render_template('index.html',prediction_text="Yes")
+      
+    else:
+      
+      return render_template('index.html',prediction_text="No")
+
+    
+if __name__=="__main__":
+    f1.run(debug=True)
